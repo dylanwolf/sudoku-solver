2f895be2fec6798c82f03bbd27b28143b187a24f
Dylan Wolf <dylan@dylanwolf.com>
2017-11-06 01:10:51 -0500

    Added ability to isolate sets of possible numbers for harder puzzles

--- sudoku.py
+++ sudoku.py
@@ -115,6 +115,18 @@ def get_group(sdku, x, y, includeCell):
 	
 	return result
 
+def get_group_by_number(sdku, index):
+
+	row = math.floor(index / 3.0)
+	col = index % 3
+	
+	result = []
+	for y in range(row * 3, (row+1) * 3):
+		for x in range(col * 3, (col+1) * 3):
+			result.append(sdku[y][x])
+			
+	return result
+	
 def calculate_possibles(sdku):
 	'''Returns a new 9x9 array that contains all of the possible sudoku values for each cell in the given grid.'''
 	result = []
@@ -131,11 +143,98 @@ def calculate_possibles(sdku):
 			else:
 				impossibles = sdku[row] + get_col(sdku, col) + get_group(sdku, col, row, False)
 				possibles = [x for x in range(1, 10) if not x in impossibles]
+				possibles.sort()
 						
 			result_row.append(possibles)
 			
 		result.append(result_row)
+		
+	has_changed = True
+	while has_changed:
+		has_changed = False
+		for i in range(0, 9):
+			if remove_row_multiples(result[i]):
+				has_changed = True
+			if remove_col_multiples(result, i):
+				has_changed = True
+			if remove_group_multiples(result, i):
+				has_changed = True
+			
+		
 	return result
+
+def count_in(array, value):
+	return len([x for x in array if x == value])
+
+	
+def remove_row_multiples(row_possibles):
+	''' For any N rows that have exactly the same set of possibles, remove from all other possibles in that set. '''
+	duplicates = [x for x in row_possibles if count_in(row_possibles, x) == len(x) and len(x) > 1]
+	
+	if not duplicates:
+		return False
+		
+	has_changed = False
+	for possibles in row_possibles:
+		for d in duplicates:
+		
+			if possibles == d:
+				continue
+		
+			for n in d:
+				if n in possibles:
+					possibles.remove(n)
+					has_changed = True
+					
+	return has_changed
+
+def remove_col_multiples(possibles, col):
+	''' For any N columns that have exactly the same set of possibles, remove from all other possibles in that set. '''
+	col_possibles = get_col(possibles, col)
+	duplicates = [x for x in col_possibles if count_in(col_possibles, x) == len(x) and len(x) > 1]
+	
+	if not duplicates:
+		return False
+		
+	has_changed = False
+	for row in range(0, 9):
+		for d in duplicates:
+		
+			if possibles[row][col] == d:
+				continue
+		
+			for n in d:
+				if n in possibles[row][col]:
+					possibles[row][col].remove(n)
+					has_changed = True
+					
+	return has_changed
+	
+def remove_group_multiples(possibles, grp):
+	''' For any N group items that have exactly the same set of possibles, remove from all other possibles in that set. '''
+	grp_possibles = get_group_by_number(possibles, grp)
+	duplicates = [x for x in grp_possibles if count_in(grp_possibles, x) == len(x) and len(x) > 1]
+	
+	if not duplicates:
+		return False
+		
+	row = math.floor(grp / 3.0)
+	col = grp % 3
+	
+	has_changed = False
+	for y in range(row * 3, (row+1)	* 3):
+		for x in range(col * 3, (col+1) * 3):
+			for d in duplicates:
+			
+				if possibles[y][x] == d:
+					continue
+				
+				for n in d:
+					if n in possibles[y][x]:
+						possibles[y][x].remove(n)
+						has_changed = True
+						
+	return has_changed
 	
 			
 def solve(sdku):
@@ -162,17 +261,13 @@ def solve(sdku):
 					# Number is only possible in square vs. row/col/group
 					row_possibles = collect_sets(possibles[row])
 					col_possibles = collect_sets(get_col(possibles, col))
-					grp_possibles = collect_sets(get_group(possibles, col, row, False))
-
-					
+					grp_possibles = collect_sets(get_group(possibles, col, row, False))					
 					
 					for n in possibles[row][col]:
 						if (n not in row_possibles) or (n not in col_possibles) or (n not in grp_possibles):
 							has_changed = True
 							work[row][col] = n
 							break
-		
-		
 	
 	return work
 			
